f05259eddc083053bb887523f46010dc4649b9da
Kushagra Gupta <kushagrab424@gmail.com>
2026-06-14 20:36:35 +0530

    Remove invalid execArgv flag from Worker threads

--- bot-fleet/src/generator.ts
+++ bot-fleet/src/generator.ts
@@ -45,7 +45,7 @@ export class LoadGenerator {
         targetUrl: this.targetUrl,
         targetRate
       },
-      execArgv: isTs ? ['-r', 'ts-node/register', '--max-old-space-size=48'] : ['--max-old-space-size=48']
+      execArgv: isTs ? ['-r', 'ts-node/register'] : undefined
     });
 
     worker.on('message', (msg) => {
